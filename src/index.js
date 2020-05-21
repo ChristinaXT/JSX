7405a1cb8ad217e443eae82a3c467de5c20be219
Christina Williams <christinaxtwilliams@gmail.com>
2020-05-21 13:51:40 -0400

    added text

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
+  const buttonText = { text: 'Click Me' };
   return (
     <div>
       <label className="label"  for="name">
       Enter name:
       </label>
       <input id="name"  type="text"/>
-      <button style ={{ backgroundColor: 'blue', color: 'white'}}>Submit</button>
+      <button style ={{ backgroundColor: 'blue', color: 'white'}}>
+        {buttonText.text}
+      </button>
     </div>
   )
 };
