358c5eda44bb6e8f78216487fee58110a766af5e
Christina Williams <christinaxtwilliams@gmail.com>
2020-05-21 13:33:41 -0400

    added button

--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-  return <div>Hi there! </div>;
+  return (
+    <div>
+      <label> Enter name: </label>
+      <input id="name" />
+      <button style ={{ backgroundColor: 'blue', color: 'white'}}>Submit</button>
+    </div>
+  )
 };
 
 // Take the react component and show it on the screen
